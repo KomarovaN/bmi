08190149942552f24675a828d321c54538c263c4
Nadezhda Komarova <grukhinan@gmail.com>
2022-11-20 16:10:54 +0300

    Fixed grammatical error

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         double weight = 62.5;
-        double heigh = 1.7;
+        double height = 1.7;
         BmiService service = new BmiService();
-        double bmi = service.calculation(weight, heigh);
+        double bmi = service.calculation(weight, height);
         if (bmi == 0) {
             System.out.println("Введите рост в метрах.");
         }
