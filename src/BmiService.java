b01553f65270a6642c7e7aefcd7152546c566612
Nadezhda Komarova <grukhinan@gmail.com>
2022-11-27 16:08:40 +0300

    Work of errors

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,12 +1,12 @@
 public class BmiService {
-    public double calculation(double m, double h) {
+    public double calculation(double weight, double hight) {
       // Индекс массы тела измеряется в кг/м² и рассчитывается по формуле: ИМТ = m/h2,
       //  где: m — масса тела в килограммах, h — рост в метрах.
-        if (h != 0) {
-            if (h > 50) {
+        if (hight != 0) {
+            if (hight > 50) {
                 return 0;
             }
-            return m / (h * h);
+            return weight / (hight * hight);
         }
         return 0;
     }
