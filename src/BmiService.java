2cace068c48211ae990251135a25c3496e610b86
Nadezhda Komarova <grukhinan@gmail.com>
2022-11-27 16:20:56 +0300

    Fixed grammatical error

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,12 +1,12 @@
 public class BmiService {
-    public double calculation(double weight, double hight) {
+    public double calculation(double weight, double height) {
       // Индекс массы тела измеряется в кг/м² и рассчитывается по формуле: ИМТ = m/h2,
       //  где: m — масса тела в килограммах, h — рост в метрах.
-        if (hight != 0) {
-            if (hight > 50) {
+        if (height != 0) {
+            if (height > 50) {
                 return 0;
             }
-            return weight / (hight * hight);
+            return weight / (height * height);
         }
         return 0;
     }
